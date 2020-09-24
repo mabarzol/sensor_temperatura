216013c11a128d1718877193ba1b74c6bcdf86a9
mabarzol <mario0barzola@gmail.com>
2020-09-24 11:30:52 -0500

    Codigo para medir la temperatura a traves dl pic16f628a y sensor de temperatura ds18

---
+++ sensor.asm
@@ -0,0 +1,8 @@
+
+_main:
+
+;sensor.c,1 :: 		void main() {
+;sensor.c,3 :: 		}
+L_end_main:
+	GOTO       $+0
+; end of _main
---
+++ sensor.cp
@@ -0,0 +1,4 @@
+#line 1 "C:/Users/PC/Desktop/sensor de temperatura/sensor.c"
+void main() {
+
+}
